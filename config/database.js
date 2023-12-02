ca78c6a3effbd81121325b73517a90f212268264
LaLoc <tususra173@gmail.com>
2023-12-02 18:48:50 +0900

    new

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'store'),
-      user: env('DATABASE_USERNAME', 'root'),
+      user: env('DATABASE_USERNAME', 'admin'),
       password: env('DATABASE_PASSWORD', 'admin'),
       ssl: env.bool('DATABASE_SSL', false),
     },
